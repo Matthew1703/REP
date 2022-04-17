9223323d854f2424c18f132ec3ffa9b77dd877e6
Матвей <matvey@MacBook-Pro-Matvej.local>
2022-04-17 23:12:23 +0500

    heromove

---
+++ Assets/heromove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heromove : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    [SerializeField] int lives = 5;
+    [SerializeField] float jumpForce = 15f;
+
+    public Rigidbody2D rb;
+    private float moveInput;
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
